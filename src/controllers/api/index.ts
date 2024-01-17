89039024a6291cce9492ba85d26ce56ac5f34a44
Miha <stele1000@gmail.com>
2024-01-17 10:57:48 +0100

    Node cache and consisntency with ;

--- src/controllers/api/index.ts
+++ src/controllers/api/index.ts
@@ -1,68 +1,81 @@
 import {Request, Response} from "express";
 import {Game, Player} from "../../db/dbinit";
-
 import {ApiRoutes} from "../../routes/api/routingSignature";
+import NodeCache from "node-cache";
 
+
+const myCache = new NodeCache();
+const cacheKey = "defaulyplayersList";
 
 const controller: ApiRoutes = {};
 
 controller.listPlayers = async (req: Request, res: Response, next) => {
-    const players: any = await Player.findAll()
 
-    return res.status(200).json({players})
+
+    const cacheContent = myCache.get(cacheKey);
+
+    if (cacheContent) {
+        return res.status(200).json({players: cacheContent});
+    } else {
+        const players: any = await Player.findAll();
+        myCache.set(cacheKey, players);
+        return res.status(200).json({players});
+    }
 }
 
 controller.getPlayer = async (req: Request, res: Response, next) => {
-    const {id} = req.params
+    const {id} = req.params;
 
     const player: any = await Player.findOne({
         where: {
             id
         }
-    })
+    });
 
-    return res.status(200).json({player})
+    return res.status(200).json({player});
 }
 
 controller.updatePlayer = async (req: Request, res: Response, next) => {
-    const {id} = req.params
+    const {id} = req.params;
 
-    const {name, surname, birthdate} = req.body
+    const {name, surname, birthdate} = req.body;
 
     const player: any = await Player.findOne({
         where: {
             id
         }
-    })
+    });
 
-    player.name = name || player.name
-    player.surname = surname || player.surname
-    player.birthdate = birthdate || player.birthdate
+    player.name = name || player.name;
+    player.surname = surname || player.surname;
+    player.birthdate = birthdate || player.birthdate;
 
-    await player.save()
+    await player.save();
+
+    myCache.del(cacheKey);
 
     return res.status(200).json({player})
 }
 
 controller.removePlayer = async (req: Request, res: Response, next) => {
-    const {id} = req.params
+    const {id} = req.params;
 
     const player: any = await Player.findOne({
         where: {
             id
         }
-    })
+    });
 
-    await player.destroy()
+    await player.destroy();
 
-    return res.status(200).json({player})
+    return res.status(200).json({player});
 }
 
 controller.addPlayer = async (req: Request, res: Response, next) => {
-    const games: any = await Game.findAll()
+    const games: any = await Game.findAll();
     // const players: any = await Player.findAll()
 
-    const {name, surname, birthdate} = req.body
+    const {name, surname, birthdate} = req.body;
 
 
     // const newGame = await Game.create({
@@ -88,76 +101,76 @@ controller.addPlayer = async (req: Request, res: Response, next) => {
 
     return res.status(200).json({
         newPlayer,
-    })
+    });
 }
 
 
 controller.listGames = async (req: Request, res: Response, next) => {
-    const games: any = await Game.findAll()
+    const games: any = await Game.findAll();
 
-    return res.status(200).json({games})
+    return res.status(200).json({games});
 }
 
 controller.getGame = async (req: Request, res: Response, next) => {
-    const {id} = req.params
+    const {id} = req.params;
 
     const game: any = await Game.findOne({
         where: {
             id
         }
-    })
+    });
 
-    return res.status(200).json({game})
+    return res.status(200).json({game});
 }
 
 controller.updateGame = async (req: Request, res: Response, next) => {
-    const {id} = req.params
+    const {id} = req.params;
 
-    const {title, description} = req.body
+    const {title, description} = req.body;
 
     const game: any = await Game.findOne({
         where: {
             id
         }
-    })
+    });
 
-    game.title = title || game.title
-    game.description = description || game.description
+    game.title = title || game.title;
+    game.description = description || game.description;
 
-    await game.save()
+    await game.save();
 
-    return res.status(200).json({game})
+    return res.status(200).json({game});
 }
 
 controller.removeGame = async (req: Request, res: Response, next) => {
-    const {id} = req.params
+    const {id} = req.params;
 
     const game: any = await Game.findOne({
         where: {
             id
         }
-    })
+    });
 
-    await game.destroy()
+    await game.destroy();
 
-    return res.status(200).json({game})
+    return res.status(200).json({game});
 }
 
 controller.addGame = async (req: Request, res: Response, next) => {
 
 
-    console.log(`Request body: ${JSON.stringify(req.body)}`)
+    console.log(`Request body: ${JSON.stringify(req.body)}`);
 
-    const {title, description} = req.body
+    const {title, description} = req.body;
 
     const newGame = await Game.create({
         title,
         description,
     });
 
-    await newGame.save()
+    await newGame.save();
 
-    return res.status(200).json({newGame})
+    return res.status(200).json({newGame});
 }
 
 
