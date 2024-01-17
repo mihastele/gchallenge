07a1c1d0836ecba15a7d8d6cdd15f920b20f9e89
Miha <stele1000@gmail.com>
2024-01-17 11:32:11 +0100

    Async error handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import path from "node:path";
 import bodyParser from 'body-parser';
+import 'express-async-errors';
 
-const express = require('express');
+import express from "express";
+
 const app = express();
 const port = 3000;
 const indexRouter = require('./routes');
+
 
 
 app.use('/', express.static(path.join(__dirname, './public')));
@@ -21,6 +24,11 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', indexRouter);
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.error(err.stack); // Log error stack trace
+    res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
     console.log(`Gamanza Challenge app listening on port ${port}`);
